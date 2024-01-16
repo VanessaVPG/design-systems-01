7b78cbf2671440bd6a807cf6775f30c397c69b69
vanessavpg <vanessa.pg026@gmail.com>
2024-01-16 14:10:40 -0300

    fix: grid-cols from MultiStep component

--- packages/react/src/components/MultiStep.tsx
+++ packages/react/src/components/MultiStep.tsx
@@ -6,14 +6,29 @@ export type MultiStepProps = {
   currentStep?: number
 }
 
-export function MultiStep({ size = 0, currentStep = 1 }: MultiStepProps) {
+export function MultiStep({ size = 4, currentStep = 1 }: MultiStepProps) {
   return (
     <div className="w-full">
       <Text className="text-gray200" variants="xs">
         Passo {currentStep} de {size}
       </Text>
 
-      <div className={`grid grid-cols-${size} gap-2 mt-1`}>
+      <div
+        className={clsx('grid gap-2 mt-1', {
+          'grid-cols-1': size === 1,
+          'grid-cols-2': size === 2,
+          'grid-cols-3': size === 3,
+          'grid-cols-4': size === 4,
+          'grid-cols-5': size === 5,
+          'grid-cols-6': size === 6,
+          'grid-cols-7': size === 7,
+          'grid-cols-8': size === 8,
+          'grid-cols-9': size === 9,
+          'grid-cols-10': size === 10,
+          'grid-cols-11': size === 11,
+          'grid-cols-12': size === 12,
+        })}
+      >
         {Array.from({ length: size }, (_, index) => index + 1).map((step) => {
           return (
             <div
